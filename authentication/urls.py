f2e66957108df4c7f28ced8a9107abbf79dcc875
Brandon Lee <brandon.sangmin.lee@gmail.com>
2021-03-03 11:22:18 -0500

    check

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
-app_name = 'account'
+app_name = 'authentication'
 urlpatterns = [
     path('login', LoginView.as_view(template_name="login.html"), name='login'),
     path('signup', views.signup, name='signup'),
