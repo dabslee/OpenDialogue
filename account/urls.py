6936ccda5211f282e1494a1d3202292847ec16fc
Brandon Lee <brandon.sangmin.lee@gmail.com>
2021-03-03 11:21:42 -0500

    failed change name auth to acc

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
-app_name = 'authentication'
+app_name = 'account'
 urlpatterns = [
     path('login', LoginView.as_view(template_name="login.html"), name='login'),
     path('signup', views.signup, name='signup'),
